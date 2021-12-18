e84901d4cb30e304985a324419b9a9acad22ba76
matthung0807 <bronx0807@gmail.com>
2021-12-18 23:08:02 +0800

    value types

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # terraform-demo
 
-[Terraform local_file hello world](https://matthung0807.blogspot.com/2021/10/terraform-localfile-hello-world.html)
+[Terraform 資料型態範例 value types example](https://matthung0807.blogspot.com/2021/11/terraform-value-types.html)
--- hello.tf
+++ hello.tf
@@ -1,4 +1,21 @@
 resource "local_file" "hello" {
     filename = "${path.module}/hello.txt"
-    content  = "Hello world!"
+    content  = local.content
+}
+
+locals {
+    name = "John"
+    age = 33
+    manager = true
+    ext = [331, 332]
+    dept = {
+        name = "IT"
+        employees = 5
+    }
+
+    content = <<-EOT
+            Hello ${local.name}, ${local.age} years old.
+            ${local.manager ? "A manager" : "An employee"} of ${local.dept.name} department with ${local.dept.employees} employees.
+            Extensions are ${join(", ", local.ext)}.
+            EOT
 }
